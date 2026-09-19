847774851cce3e93c60d51d7558aa1cee59ccfa0
arielgfs <johanngiven@gmail.com>
2026-09-19 22:38:42 +0800

    don

--- posttest/posttest-apd-2/2609106013-M_Al-Kahfi_Hafidz_M-PT-2.py
+++ posttest/posttest-apd-2/2609106013-M_Al-Kahfi_Hafidz_M-PT-2.py
@@ -9,13 +9,15 @@ barang = [barang_1, barang_2, barang_3, barang_4, barang_5, barang_6]
 total_belanja = (barang[0] + barang[1] + barang[2] + barang[3] + barang[4] + barang[5])
 pajak = total_belanja * 0.15
 total_bayar = total_belanja + pajak
+
+
+rata_rata = total_bayar / len(barang)
+nim = 13
+bolean = nim < rata_rata
+
 total_euro = total_bayar / 20455
 total_yuan = total_bayar / 2659
 
-rata_rata = total_bayar / len(barang)
-
-nim = 13
-bolean = nim < rata_rata
 
 print("isi variabel barang:", barang)
 print("total belanja:", total_belanja)
